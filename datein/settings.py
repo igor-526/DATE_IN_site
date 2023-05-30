d3e0fbe5f8918a19322bf494ff1d1b6787580f0d
Igor <django@igor-5261.fvds.ru>
2023-05-30 15:35:50 +0300

    Successfully deployed

--- datein/settings.py
+++ datein/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-!&!*%+_b#wdkr2knz36ngyc$xwdd*+554jd9c_3!174n=$#z^t
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'datein.ru', '80.87.192.255']
 
 
 # Application definition
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'datein',
         'USER': 'postgres',
-        'PASSWORD': '3611810700',
+        'PASSWORD': 'C5uGTeyE',
         'HOST': 'localhost',
         'PORT': '5432'
     }
@@ -123,10 +123,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-   os.path.join(BASE_DIR, "static"),
-]
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
+#STATICFILES_DIRS = [
+#   os.path.join(BASE_DIR, "static"),
+#]
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
